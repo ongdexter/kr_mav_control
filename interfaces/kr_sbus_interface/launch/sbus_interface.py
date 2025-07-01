ca9c2d600428be97d4d2ec132e5a19145af39f9a
Dexter <dexterong94@gmail.com>
2025-07-01 16:36:23 +0000

    Initial ros2 port of sbus_interface

---
+++ interfaces/kr_sbus_interface/launch/sbus_interface.py
@@ -0,0 +1,57 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+def generate_launch_description():
+    # Declare launch arguments
+    robot_ns   = LaunchConfiguration('robot',   default='/')
+    odom_topic = LaunchConfiguration('odom',    default='odom')
+    so3_cmd_topic  = LaunchConfiguration('so3_cmd', default='so3_cmd')
+
+    # Path to the configuration file
+    config_file = os.path.join(
+        get_package_share_directory('kr_sbus_interface'),
+        'config',
+        'neurofly.yaml'
+    )
+
+    # Component container to load your SO3CmdToSbus component
+    sbus_container = ComposableNodeContainer(
+        name='so3cmd_to_sbus_container',
+        namespace=robot_ns,
+        package='rclcpp_components',
+        executable='component_container_mt',  # multi-threaded
+        composable_node_descriptions=[
+            ComposableNode(
+                package='kr_sbus_interface',
+                plugin='SO3CmdToSBUS',
+                name='so3cmd_to_sbus',
+                remappings=[
+                    ('odom',   odom_topic),
+                    ('so3_cmd', so3_cmd_topic),
+                ],
+                parameters=[config_file, {'port_name': '/dev/ttyTHS0'}],
+            ),
+        ],
+        output='screen',
+        )
+    robot_arg = DeclareLaunchArgument(
+        'robot', default_value=''
+    )
+    odom_arg = DeclareLaunchArgument(
+        'odom', default_value='odom'
+    )
+    so3_cmd_arg = DeclareLaunchArgument(
+        'so3_cmd', default_value='so3_cmd'
+    )
+    return LaunchDescription([
+        robot_arg,
+        odom_arg,
+        so3_cmd_arg,
+        sbus_container,
+    ])
